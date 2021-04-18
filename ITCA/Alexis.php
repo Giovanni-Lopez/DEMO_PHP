4b048e4b9086e912a6d5946b620532722833645f
unknown <alexisange001@gmail.com>
2021-04-17 22:06:04 -0600

    Creacion del directorio ITCA y ficheros PHP

---
+++ ITCA/Alexis.php
@@ -0,0 +1,5 @@
+<?php 
+
+	echo "Mi nombre completo es: Alexis Giovanni Angel López";
+	echo "El trabajo con git y control de versiones me parece una opcion buena para poner en practica";
+?>
---
+++ ITCA/López.php
@@ -0,0 +1,5 @@
+<?php 
+	echo "Me gusta que me digan GIOVANNI";
+	echo "Debemos de aprender a practicar el Contol de versiones ya que parece muy buena opcion a tomar en cuenta al momento de programar algun programa";
+
+?>
